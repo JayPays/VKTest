2a1ef7ca3c6a60d16cebc287c0801d1037e88482
Jay <jay@imac-jay.local>
2016-07-02 12:44:14 +0500

    Initial Commit

---
+++ VKTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VKTask
+//
+//  Created by Jay on 02.07.16.
+//  Copyright © 2016 Vladislav Molodetskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VKTask/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VKTask
+//
+//  Created by Jay on 02.07.16.
+//  Copyright © 2016 Vladislav Molodetskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
